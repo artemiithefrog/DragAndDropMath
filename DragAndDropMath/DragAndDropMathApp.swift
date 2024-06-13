7faeec6b521bc5177819b0ecaf17cfebdede96bc
Artemii Malyshev <115695294+artemiithefrog@users.noreply.github.com>
2024-06-13 19:24:13 +0200

    Initial Commit

---
+++ DragAndDropMath/DragAndDropMathApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragAndDropMathApp.swift
+//  DragAndDropMath
+//
+//  Created by artemiithefrog . on 13.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct DragAndDropMathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
